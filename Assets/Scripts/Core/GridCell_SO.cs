32160efd29f22ef5346528f0d7d5a6c093493c2b
davidkuc <91789757+davidkuc@users.noreply.github.com>
2022-12-03 20:36:21 +0100

    Added zenject, project context and scene context

---
+++ Assets/Scripts/Core/GridCell_SO.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class GridCell_SO : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
