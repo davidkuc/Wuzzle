2e0323097d893cc2f332b7c0efca74e461eea72c
davidkuc <91789757+davidkuc@users.noreply.github.com>
2022-12-04 12:17:05 +0100

    Lol snapping to grid cell works!

--- Assets/Scripts/Other/Draggable.cs
+++ Assets/Scripts/Other/Draggable.cs
@@ -7,6 +7,22 @@ public class Draggable : Debuggable
     private Camera camera;
     private Vector3 mousePositionOffset;
 
+    private bool isOverGridCell;
+    private GridCell currentGridCell;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isOverGridCell = true;
+        currentGridCell = collision.GetComponent<GridCell>();
+        PrintDebugLog("TriggerEnter!");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isOverGridCell= false;
+        PrintDebugLog("TriggerExit!");
+    }
+
     [Inject]
     public void Setup(Camera camera) => this.camera = camera;
 
@@ -20,6 +36,11 @@ public class Draggable : Debuggable
     {
         PrintDebugLog("MouseDrag!");
         transform.position = GetMouseWorldPosition() + mousePositionOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        transform.position = currentGridCell.transform.position;
     }
 
     private Vector3 GetMouseWorldPosition() => camera.ScreenToWorldPoint(Input.mousePosition);
